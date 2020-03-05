c6802ea06f1e8e50dfe56e9b3d5eca243a4bbc32
ivotai <ivotai@163.com>
2020-03-05 14:35:32 +0800

    公告列表。

--- app/src/main/java/com/unicorn/forensic2/ui/act/XtggListAct.kt
+++ app/src/main/java/com/unicorn/forensic2/ui/act/XtggListAct.kt
@@ -1,13 +1,12 @@
 package com.unicorn.forensic2.ui.act
 
 import com.chad.library.adapter.base.BaseQuickAdapter
-import com.unicorn.forensic2.app.defaultPadding
+import com.unicorn.forensic2.app.addDefaultItemDecoration
 import com.unicorn.forensic2.data.model.Page
 import com.unicorn.forensic2.data.model.Xtgg
 import com.unicorn.forensic2.ui.adapter.XtggAdapter
 import com.unicorn.forensic2.ui.base.KVHolder
 import com.unicorn.forensic2.ui.base.SimplePageAct
-import com.unicorn.forensic2.ui.other.decoration.LinearSpanDecoration
 import io.reactivex.Single
 import kotlinx.android.synthetic.main.ui_title_swipe_recycler.*
 
@@ -16,7 +15,7 @@ class XtggListAct : SimplePageAct<Xtgg, KVHolder>() {
     override fun initViews() {
         super.initViews()
         titleBar.setTitle("系统公告")
-        recyclerView.addItemDecoration(LinearSpanDecoration(defaultPadding))
+        recyclerView.addDefaultItemDecoration(1)
     }
 
     override val simpleAdapter: BaseQuickAdapter<Xtgg, KVHolder> = XtggAdapter()
--- app/src/main/java/com/unicorn/forensic2/ui/adapter/XtggAdapter.kt
+++ app/src/main/java/com/unicorn/forensic2/ui/adapter/XtggAdapter.kt
@@ -2,12 +2,20 @@ package com.unicorn.forensic2.ui.adapter
 
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.unicorn.forensic2.R
+import com.unicorn.forensic2.app.displayDateFormat
 import com.unicorn.forensic2.data.model.Xtgg
 import com.unicorn.forensic2.ui.base.KVHolder
+import kotlinx.android.synthetic.main.item_xtgg.*
+import org.joda.time.DateTime
+import java.util.*
 
 class XtggAdapter : BaseQuickAdapter<Xtgg, KVHolder>(R.layout.item_xtgg) {
 
     override fun convert(helper: KVHolder, item: Xtgg) {
+        helper.apply {
+            tvTitle.text = item.title
+            tvDatePublished.text = DateTime(Date(item.datepublished)).toString(displayDateFormat)
+        }
     }
 
 }
