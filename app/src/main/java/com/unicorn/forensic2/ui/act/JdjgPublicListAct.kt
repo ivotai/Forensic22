3affae79da7bd1b0c051c167494630f05b761d81
ivotai <ivotai@163.com>
2020-03-11 10:07:35 +0800

    区分鉴定机构的 public 和 my

--- app/src/main/java/com/unicorn/forensic2/ui/act/JdjgPublicListAct.kt
+++ app/src/main/java/com/unicorn/forensic2/ui/act/JdjgPublicListAct.kt
@@ -4,14 +4,14 @@ import com.chad.library.adapter.base.BaseQuickAdapter
 import com.unicorn.forensic2.app.defaultPadding
 import com.unicorn.forensic2.data.model.Jdjg
 import com.unicorn.forensic2.data.model.Page
-import com.unicorn.forensic2.ui.adapter.JdjgAdapter
+import com.unicorn.forensic2.ui.adapter.JdjgPublicAdapter
 import com.unicorn.forensic2.ui.base.KVHolder
 import com.unicorn.forensic2.ui.base.SimplePageAct
 import com.unicorn.forensic2.ui.other.decoration.LinearSpanDecoration
 import io.reactivex.Single
 import kotlinx.android.synthetic.main.ui_title_swipe_recycler.*
 
-class JdjgListAct : SimplePageAct<Jdjg, KVHolder>() {
+class JdjgPublicListAct : SimplePageAct<Jdjg, KVHolder>() {
 
     override fun initViews() {
         super.initViews()
@@ -19,7 +19,7 @@ class JdjgListAct : SimplePageAct<Jdjg, KVHolder>() {
         mRecyclerView.addItemDecoration(LinearSpanDecoration(defaultPadding))
     }
 
-    override val simpleAdapter: BaseQuickAdapter<Jdjg, KVHolder> = JdjgAdapter()
+    override val simpleAdapter: BaseQuickAdapter<Jdjg, KVHolder> = JdjgPublicAdapter()
 
     override fun loadPage(page: Int): Single<Page<Jdjg>> = v1Api.getJdjgList(page = page)
 
--- app/src/main/java/com/unicorn/forensic2/ui/adapter/HomeMenuAdapter.kt
+++ app/src/main/java/com/unicorn/forensic2/ui/adapter/HomeMenuAdapter.kt
@@ -8,7 +8,7 @@ import com.unicorn.forensic2.app.isLogin
 import com.unicorn.forensic2.app.safeClicks
 import com.unicorn.forensic2.app.startAct
 import com.unicorn.forensic2.data.model.HomeMenu
-import com.unicorn.forensic2.ui.act.JdjgListAct
+import com.unicorn.forensic2.ui.act.JdjgPublicListAct
 import com.unicorn.forensic2.ui.act.MyJdjgAct
 import com.unicorn.forensic2.ui.act.TsjyAct
 import com.unicorn.forensic2.ui.act.XtggListAct
@@ -25,7 +25,7 @@ class HomeMenuAdapter : BaseQuickAdapter<HomeMenu, KVHolder>(R.layout.item_home_
         helper.apply {
             root.safeClicks().subscribe {
                 when (item) {
-                    HomeMenu.JGCX -> mContext.startAct(JdjgListAct::class.java)
+                    HomeMenu.JGCX -> mContext.startAct(JdjgPublicListAct::class.java)
                     HomeMenu.XTGG -> mContext.startAct(XtggListAct::class.java)
                     HomeMenu.TSJY -> {
                         if (!isLogin)
