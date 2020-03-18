a8135d0393d09ffe06c4aa01aabb9063a393186e
ivotai <ivotai@163.com>
2020-03-18 14:21:56 +0800

    审核记录列表

--- app/src/main/java/com/unicorn/forensic2/ui/adapter/ShjlJdjgAdapter.kt
+++ app/src/main/java/com/unicorn/forensic2/ui/adapter/ShjlJdjgAdapter.kt
@@ -12,9 +12,18 @@ class ShjlJdjgAdapter : BaseQuickAdapter<ShjlJdjg, KVHolder>(R.layout.item_shjl_
     override fun convert(helper: KVHolder, item: ShjlJdjg) {
         helper.apply {
             tvJgmc.text = item.jgmc
-            tvDate.text = item.lastUpdateDate.toDisplayFormat()
+            tvDate.text = item.createdDate.toDisplayFormat()
             tvRemark.text = item.remark
-            tvResult.text = if (item.dataflag == 1) "通过" else "不通过"
+            tvResult.text = when (item.dataflag) {
+                1 -> "待审核"
+                2 -> "审核通过"
+                3 -> "审核不通过"
+                4 -> "待公示"
+                5 -> "公示中"
+                6 -> "公示通过"
+                7 -> "公示不通过"
+                else -> "赞同"
+            }
         }
     }
 
