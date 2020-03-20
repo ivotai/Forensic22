170a4eaf19a0ef21e5e7f7dcdb4b3d7bf55c50ba
ivotai <ivotai@163.com>
2020-03-20 11:26:13 +0800

    case item 调整。

--- app/src/main/java/com/unicorn/forensic2/ui/adapter/CaseAdapter.kt
+++ app/src/main/java/com/unicorn/forensic2/ui/adapter/CaseAdapter.kt
@@ -14,9 +14,12 @@ class CaseAdapter : BaseQuickAdapter<Case, KVHolder>(R.layout.item_case) {
 
     override fun convert(helper: KVHolder, item: Case) {
         helper.apply {
-            tvJdNo.text = "鉴定案号：${item.jdNo}"
-//            tvLarq.text = "立案日期：${DateTime(item.larq).toString(displayDateFormat2)}"
-//            tvCaseNo.text = "案件案号：${item.caseNo}"
+            tvJdNo.text = item.jdNo
+            tvCourtNameApply.text = item.courtNameApply
+            tvCaseType.text = item.caseType
+            tvCaseStatus.text = item.caseStatus
+            tvCaseNo.text = item.caseNo
+            tvCourtNameAccept.text = item.courtNameAccept
         }
         helper.root.safeClicks().subscribe {
             Intent(mContext, CaseDetailAct::class.java).apply {
