9b1a6054ae79150065b46fc19a8d03f8cfeec72b
ivotai <ivolianer2015@163.com>
2020-08-03 12:14:00 +0800

    案件查询。

--- app/src/main/java/com/unicorn/forensic2/ui/act/query/CaseQueryAct.kt
+++ app/src/main/java/com/unicorn/forensic2/ui/act/query/CaseQueryAct.kt
@@ -1,11 +1,10 @@
 package com.unicorn.forensic2.ui.act.query
 
-import com.afollestad.materialdialogs.MaterialDialog
-import com.afollestad.materialdialogs.datetime.datePicker
-import com.afollestad.materialdialogs.list.listItems
 import com.unicorn.forensic2.R
-import com.unicorn.forensic2.app.*
-import com.unicorn.forensic2.data.event.CaseQueryMapEvent
+import com.unicorn.forensic2.app.RxBus
+import com.unicorn.forensic2.app.safeClicks
+import com.unicorn.forensic2.app.trimText
+import com.unicorn.forensic2.data.event.QueryMapEvent
 import com.unicorn.forensic2.ui.base.BaseAct
 import kotlinx.android.synthetic.main.act_case_query.*
 
@@ -16,45 +15,44 @@ class CaseQueryAct : BaseAct() {
     }
 
     override fun bindIntent() {
-        tvLarqBegin.safeClicks().subscribe {
-            MaterialDialog(this).show {
-                datePicker { _, date ->
-                    this@CaseQueryAct.tvLarqBegin.text = date.time.time.toDisplayFormat()
-                }
-            }
-        }
-        tvLarqEnd.safeClicks().subscribe {
-            MaterialDialog(this).show {
-                datePicker { _, date ->
-                    this@CaseQueryAct.tvLarqEnd.text = date.time.time.toDisplayFormat()
-                }
-            }
-        }
-        fun showAjlxDialog() {
-            MaterialDialog(this@CaseQueryAct).show {
-                val items = ajlxList
-                listItems(items = items.map { it.name }) { _, index, _ ->
-                    flag = items[index].id
-                    this@CaseQueryAct.tvFlag.text = items[index].name
-                }
-            }
-        }
-        tvFlag.safeClicks().subscribe { showAjlxDialog() }
+//        tvLarqBegin.safeClicks().subscribe {
+//            MaterialDialog(this).show {
+//                datePicker { _, date ->
+//                    this@CaseQueryAct.tvLarqBegin.text = date.time.time.toDisplayFormat()
+//                }
+//            }
+//        }
+//        tvLarqEnd.safeClicks().subscribe {
+//            MaterialDialog(this).show {
+//                datePicker { _, date ->
+//                    this@CaseQueryAct.tvLarqEnd.text = date.time.time.toDisplayFormat()
+//                }
+//            }
+//        }
+//        fun showAjlxDialog() {
+//            MaterialDialog(this@CaseQueryAct).show {
+//                val items = ajlxList
+//                listItems(items = items.map { it.name }) { _, index, _ ->
+//                    flag = items[index].id
+//                    this@CaseQueryAct.tvFlag.text = items[index].name
+//                }
+//            }
+//        }
+//        tvFlag.safeClicks().subscribe { showAjlxDialog() }
 
         titleBar.setOperation("确认").safeClicks().subscribe {
             val queryMap = HashMap<String, Any>()
-            if (tvLarqBegin.isNotBlack())
-                queryMap["larqBegin"] = tvLarqBegin.trimText()
-            if (tvLarqEnd.isNotBlack())
-                queryMap["larqEnd"] = tvLarqEnd.trimText()
-            if (flag.isNotBlank())
-                queryMap["flag"] = flag
-            RxBus.post(CaseQueryMapEvent(queryMap = queryMap))
+            queryMap["ah"] = tvAh.trimText()
+            queryMap["year"]= tvYear.trimText()
+            queryMap["plaintiff"]= tvPlaintiff.trimText()
+//            if (tvLarqBegin.isNotBlack())
+//                queryMap["larqBegin"] = tvLarqBegin.trimText()
+//            if (flag.isNotBlank())
+//                queryMap["flag"] = flag
+            RxBus.post(QueryMapEvent(queryMap = queryMap))
             finish()
         }
     }
-
-    private var flag = ""
 
     override val layoutId = R.layout.act_case_query
 
--- app/src/main/java/com/unicorn/forensic2/ui/fra/CaseListFra.kt
+++ app/src/main/java/com/unicorn/forensic2/ui/fra/CaseListFra.kt
@@ -6,11 +6,14 @@ import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.unicorn.forensic2.R
 import com.unicorn.forensic2.app.RxBus
-import com.unicorn.forensic2.app.addDefaultItemDecoration
+import com.unicorn.forensic2.app.safeClicks
+import com.unicorn.forensic2.app.startAct
+import com.unicorn.forensic2.data.event.QueryMapEvent
 import com.unicorn.forensic2.data.model.Case
 import com.unicorn.forensic2.data.model.CaseType
 import com.unicorn.forensic2.data.model.CaseTypeS
 import com.unicorn.forensic2.data.model.Page
+import com.unicorn.forensic2.ui.act.query.CaseQueryAct
 import com.unicorn.forensic2.ui.adapter.CaseAdapter
 import com.unicorn.forensic2.ui.adapter.CaseTypeSAdapter
 import com.unicorn.forensic2.ui.base.KVHolder
@@ -37,6 +40,11 @@ class CaseListFra : SimplePageFra<Case, KVHolder>() {
         val data = CaseType.all.map { CaseTypeS(it) }
         data.forEach { it.isSelect = it.caseType == caseType }
         caseTypeSAdapter.setNewData(data)
+
+        titleBar.setOperation("查询").safeClicks().subscribe {
+            context?.startAct(CaseQueryAct::class.java)
+        }
+
     }
 
     override fun registerEvent() {
@@ -44,7 +52,13 @@ class CaseListFra : SimplePageFra<Case, KVHolder>() {
             caseType = it
             loadFirstPage()
         })
+        RxBus.registerEvent(this, QueryMapEvent::class.java, Consumer {
+            queryMap = it.queryMap
+            loadFirstPage()
+        })
     }
+
+    private var queryMap = HashMap<String, Any>()
 
     override val simpleAdapter: BaseQuickAdapter<Case, KVHolder> = CaseAdapter()
 
@@ -52,17 +66,17 @@ class CaseListFra : SimplePageFra<Case, KVHolder>() {
 
     override fun loadPage(page: Int): Single<Page<Case>> {
         return when (caseType) {
-            CaseType.ZBTZ -> api.getZbtzList(page = page)
-            CaseType.DJD -> api.getDjdList(page = page)
-            CaseType.YJD -> api.getYjdList(page = page)
-            CaseType.YJJ -> api.getYjjList(page = page)
-            CaseType.YXA -> api.getYxaList(page = page)
-            CaseType.CLOSE -> api.getClose(page = page)
-            CaseType.UNCLOSE -> api.getUnClose(page = page)
-            CaseType.LASP -> api.getAcceptApproval(page)
-            CaseType.CYHSP -> api.getShakeAgainApproval(page)
-            CaseType.JASP -> api.getCloseApproval(page)
-            CaseType.XASP -> api.getDestroyApproval(page)
+            CaseType.ZBTZ -> api.getZbtzList(page = page, queryMap = queryMap)
+            CaseType.DJD -> api.getDjdList(page = page, queryMap = queryMap)
+            CaseType.YJD -> api.getYjdList(page = page, queryMap = queryMap)
+            CaseType.YJJ -> api.getYjjList(page = page, queryMap = queryMap)
+            CaseType.YXA -> api.getYxaList(page = page, queryMap = queryMap)
+            CaseType.CLOSE -> api.getClose(page = page, queryMap = queryMap)
+            CaseType.UNCLOSE -> api.getUnClose(page = page, queryMap = queryMap)
+            CaseType.LASP -> api.getAcceptApproval(page = page, queryMap = queryMap)
+            CaseType.CYHSP -> api.getShakeAgainApproval(page = page, queryMap = queryMap)
+            CaseType.JASP -> api.getCloseApproval(page = page, queryMap = queryMap)
+            CaseType.XASP -> api.getDestroyApproval(page = page, queryMap = queryMap)
         }
     }
 
@@ -77,4 +91,5 @@ class CaseListFra : SimplePageFra<Case, KVHolder>() {
         get() = swipeRefreshLayout
 
     override val layoutId = R.layout.fra_case_list
+
 }
