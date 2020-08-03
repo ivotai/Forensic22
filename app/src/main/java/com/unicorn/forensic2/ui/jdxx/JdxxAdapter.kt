7b12eb62abb34300b60e26bab160c643e1fbc4f0
ivotai <ivolianer2015@163.com>
2020-08-03 14:30:50 +0800

    Jdxx 布局。

--- app/src/main/java/com/unicorn/forensic2/ui/jdxx/JdxxAdapter.kt
+++ app/src/main/java/com/unicorn/forensic2/ui/jdxx/JdxxAdapter.kt
@@ -2,19 +2,20 @@ package com.unicorn.forensic2.ui.jdxx
 
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.unicorn.forensic2.R
+import com.unicorn.forensic2.app.toDisplayFormat
 import com.unicorn.forensic2.ui.base.KVHolder
-import kotlinx.android.synthetic.main.item_case.*
+import kotlinx.android.synthetic.main.item_jdxx.*
 
 class JdxxAdapter : BaseQuickAdapter<Jdxx, KVHolder>(R.layout.item_jdxx) {
 
     override fun convert(helper: KVHolder, item: Jdxx) {
         helper.apply {
-            tvJdNo.text = item.jdNo
-            tvCourtNameApply.text = item.courtNameApply
-            tvCaseType.text = item.caseType
-            tvCaseStatus.text = item.caseStatus
-            tvCaseNo.text = item.caseNo
-            tvCourtNameAccept.text = item.courtNameAccept
+            tvJdlb.text = item.jdlb
+            tvDateLottery.text = item.dateLottery.toDisplayFormat()
+            tvJgmc.text = item.jgmc
+            tvZbtz.text = item.fidzbtz?.filename ?: ""
+            tvYhhf.text = item.fidyhhf
+            tvJdbg.text = ""// todo fidjdbg
         }
     }
 
