4848b74d6128ca5c8b884f3da8c95993ab8f30ea
ivotai <ivolianer2015@163.com>
2020-08-03 18:13:44 +0800

    已消案，已拒绝，无法查看详情。

--- app/src/main/java/com/unicorn/forensic2/ui/adapter/CaseAdapter.kt
+++ app/src/main/java/com/unicorn/forensic2/ui/adapter/CaseAdapter.kt
@@ -6,8 +6,10 @@ import com.unicorn.forensic2.R
 import com.unicorn.forensic2.app.Param
 import com.unicorn.forensic2.app.safeClicks
 import com.unicorn.forensic2.data.model.Case
+import com.unicorn.forensic2.data.model.CaseType
 import com.unicorn.forensic2.ui.act.CaseDetailAct
 import com.unicorn.forensic2.ui.base.KVHolder
+import com.unicorn.forensic2.ui.fra.CaseListFra
 import kotlinx.android.synthetic.main.item_case.*
 
 class CaseAdapter : BaseQuickAdapter<Case, KVHolder>(R.layout.item_case) {
@@ -22,6 +24,7 @@ class CaseAdapter : BaseQuickAdapter<Case, KVHolder>(R.layout.item_case) {
             tvCourtNameAccept.text = item.courtNameAccept
         }
         helper.root.safeClicks().subscribe {
+            if (CaseListFra.caseType in listOf(CaseType.YJJ, CaseType.YXA)) return@subscribe
             Intent(mContext, CaseDetailAct::class.java).apply {
                 putExtra(Param, item)
             }.let { mContext.startActivity(it) }
