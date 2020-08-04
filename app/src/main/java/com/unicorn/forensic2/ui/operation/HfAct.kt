828d0ad8eaf7b8baaa5be0e802701fb40bdd9c48
ivotai <ivolianer2015@163.com>
2020-08-04 09:56:55 +0800

    Hf 界面。

--- app/src/main/java/com/unicorn/forensic2/ui/operation/HfAct.kt
+++ app/src/main/java/com/unicorn/forensic2/ui/operation/HfAct.kt
@@ -1,6 +1,10 @@
 package com.unicorn.forensic2.ui.operation
 
+import android.view.View
+import android.widget.RadioButton
+import com.jakewharton.rxbinding3.widget.checkedChanges
 import com.unicorn.forensic2.R
+import com.unicorn.forensic2.app.safeClicks
 import com.unicorn.forensic2.ui.base.BaseAct
 import kotlinx.android.synthetic.main.act_hf.*
 
@@ -8,6 +12,22 @@ class HfAct : BaseAct() {
 
     override fun initViews() {
         titleBar.setTitle("回复")
+    }
+
+    override fun bindIntent() {
+        rgIsAccept.checkedChanges().subscribe {
+            val radioButton = rgIsAccept.findViewById<RadioButton>(it)
+            val flag = radioButton.text == "接受"
+            etRejectInfo.visibility = if (flag) View.VISIBLE else View.GONE
+        }
+
+        titleBar.setOperation("确定").safeClicks().subscribe {
+
+        }
+    }
+
+    private fun reply(){
+
     }
 
     override val layoutId = R.layout.act_hf
