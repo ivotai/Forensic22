b8b96a7a0444a13254b0a2d591a9322115d7aefe
ivotai <ivolianer2015@163.com>
2020-08-05 14:33:16 +0800

    变测试边修改。

--- app/src/main/java/com/unicorn/forensic2/ui/operation/lotteryDelay/LotteryDelayAdapter.kt
+++ app/src/main/java/com/unicorn/forensic2/ui/operation/lotteryDelay/LotteryDelayAdapter.kt
@@ -2,7 +2,6 @@ package com.unicorn.forensic2.ui.operation.lotteryDelay
 
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.unicorn.forensic2.R
-import com.unicorn.forensic2.app.toDisplayFormat
 import com.unicorn.forensic2.app.toDisplayFormat2
 import com.unicorn.forensic2.ui.base.KVHolder
 import kotlinx.android.synthetic.main.item_lottery_delay.*
@@ -11,13 +10,13 @@ class LotteryDelayAdapter : BaseQuickAdapter<LotteryDelay, KVHolder>(R.layout.it
 
     override fun convert(helper: KVHolder, item: LotteryDelay) {
         helper.apply {
-            tvApplydttm.text = item.applyDttm.toDisplayFormat()
+            tvApplydttm.text = item.applyDttm.toDisplayFormat2()
             tvApplystate.text = item.applyStateStr
             tvFee.text = item.fee.toString()
             tvJdryxm.text = item.jdryxm
             tvTerminate.text = if (item.terminate == 0) "否" else "是"
             tvApproveBy.text =item.approveBy
-            tvApplydttm.text = item.applyDttm.toDisplayFormat2()
+            tvApproveDttm.text = item.approveDttm.toDisplayFormat2()
             tvApproveInfo.text = item.approveInfo
         }
     }
