59bb850c2a78ec9c292f62fea5c9b23298a163c4
ivotai <ivolianer2015@163.com>
2020-08-08 11:15:45 +0800

    开始 refactor.

--- app/src/main/java/com/unicorn/forensic2/ui/adapter/MainPagerAdapter.kt
+++ app/src/main/java/com/unicorn/forensic2/ui/adapter/MainPagerAdapter.kt
@@ -7,9 +7,9 @@ import androidx.viewpager.widget.PagerAdapter
 import com.unicorn.forensic2.app.isLogin
 import com.unicorn.forensic2.app.roleTag
 import com.unicorn.forensic2.data.model.Role
-import com.unicorn.forensic2.ui.fra.HomeMainFra
+import com.unicorn.forensic2.refactor.profile.ProfileFra
 import com.unicorn.forensic2.ui.fra.CaseListFra
-import com.unicorn.forensic2.ui.fra.MyMainFra
+import com.unicorn.forensic2.ui.fra.HomeMainFra
 import com.unicorn.forensic2.ui.fra.WdpsFra
 
 @Suppress("DEPRECATION")
@@ -22,7 +22,7 @@ class MainPagerAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
     override fun getItem(position: Int): Fragment = when (position) {
         0 -> HomeMainFra()
         1 -> if (roleTag == Role.Pszj.en) WdpsFra() else CaseListFra()
-        else -> MyMainFra()
+        else -> ProfileFra()
     }
 
     override fun getCount(): Int = titles.size
