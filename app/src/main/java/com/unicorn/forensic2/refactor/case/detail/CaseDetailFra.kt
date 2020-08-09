02362ac43b4f5ce76d5ce0d5bf13dcb8560d5821
ivotai <ivolianer2015@163.com>
2020-08-09 18:05:35 +0800

    CaseDetailFra.kt

---
+++ app/src/main/java/com/unicorn/forensic2/refactor/case/detail/CaseDetailFra.kt
@@ -0,0 +1,31 @@
+package com.unicorn.forensic2.refactor.case.detail
+
+import com.unicorn.forensic2.R
+import com.unicorn.forensic2.app.Param
+import com.unicorn.forensic2.app.toDisplayFormat2
+import com.unicorn.forensic2.data.model.Case
+import com.unicorn.forensic2.ui.base.BaseFra
+import kotlinx.android.synthetic.main.fra_case_detail.*
+
+class CaseDetailFra : BaseFra() {
+
+    override fun initViews() {
+
+        with(case) {
+            tvJdNo.text = jdNo
+            tvCourtNameApply.text = courtNameApply
+            tvCaseType.text = caseTypeX
+            tvCaseStatus.text = caseStatusX
+            tvCaseNo.text = caseNo
+            tvCourtNameAccept.text = courtNameAccept
+            tvDateAccept.text = dateAccept.toDisplayFormat2()
+            tvDateClose.text = dateClose.toDisplayFormat2("尚未结案")
+            tvCloseTypeDisplay.text = if (closeTypeDisplay.isEmpty()) "尚未结案" else closeTypeDisplay
+        }
+    }
+
+    private val case by lazy { arguments!!.getSerializable(Param) as Case }
+
+    override val layoutId = R.layout.fra_case
+
+}
