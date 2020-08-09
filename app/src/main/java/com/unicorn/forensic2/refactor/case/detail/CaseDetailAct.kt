1c8fd38dd7861d8a6a9841148bdd36f022e78735
ivotai <ivolianer2015@163.com>
2020-08-09 18:49:12 +0800

    调整中。

--- app/src/main/java/com/unicorn/forensic2/refactor/case/detail/CaseDetailAct.kt
+++ app/src/main/java/com/unicorn/forensic2/refactor/case/detail/CaseDetailAct.kt
@@ -2,7 +2,9 @@ package com.unicorn.forensic2.refactor.case.detail
 
 import android.content.Context
 import android.graphics.Color
+import android.graphics.drawable.GradientDrawable
 import androidx.core.content.ContextCompat
+import com.blankj.utilcode.util.ConvertUtils
 import com.unicorn.forensic2.R
 import com.unicorn.forensic2.app.Param
 import com.unicorn.forensic2.data.model.Case
@@ -14,8 +16,7 @@ import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.CommonNav
 import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.IPagerIndicator
 import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.IPagerTitleView
 import net.lucode.hackware.magicindicator.buildins.commonnavigator.indicators.LinePagerIndicator
-import net.lucode.hackware.magicindicator.buildins.commonnavigator.titles.ColorTransitionPagerTitleView
-import net.lucode.hackware.magicindicator.buildins.commonnavigator.titles.SimplePagerTitleView
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.titles.ClipPagerTitleView
 
 class CaseDetailAct : BaseAct() {
 
@@ -26,39 +27,51 @@ class CaseDetailAct : BaseAct() {
 
     val list = CaseDetailType.all
 
-    private fun initVp(){
-//        magicIndicator.setBackgroundColor(Color.parseColor("#455a64"))
+    private fun initVp() {
+        viewPaper.adapter = CaseDetailPagerAdapter(supportFragmentManager, case)
+        magicIndicator.background = GradientDrawable().apply {
+            setColor(Color.parseColor("#40000000"))
+            cornerRadius = ConvertUtils.dp2px(8f).toFloat()
+        }
+//        magicIndicator.setBackgroundResource(R.drawable.round_indicator_bg)
         val commonNavigator = CommonNavigator(this)
         commonNavigator.adapter = object : CommonNavigatorAdapter() {
             override fun getCount(): Int {
-                return  list.size
+                return list.size
             }
 
             override fun getTitleView(context: Context, index: Int): IPagerTitleView {
-                val simplePagerTitleView: SimplePagerTitleView =
-                    ColorTransitionPagerTitleView(context)
-                simplePagerTitleView.text = list[index].cn
-                simplePagerTitleView.normalColor = Color.BLACK
-                simplePagerTitleView.selectedColor =colorPrimary
-                simplePagerTitleView.setOnClickListener {  }
-                return simplePagerTitleView
+                val clipPagerTitleView = ClipPagerTitleView(context)
+                clipPagerTitleView.text = list.get(index).cn
+                clipPagerTitleView.textColor = Color.WHITE
+                clipPagerTitleView.clipColor = colorPrimary
+                clipPagerTitleView.textSize = 16f
+                clipPagerTitleView.setOnClickListener { viewPaper.setCurrentItem(index) }
+                return clipPagerTitleView
             }
 
             override fun getIndicator(context: Context): IPagerIndicator {
                 val indicator = LinePagerIndicator(context)
-                indicator.setColors(colorPrimary)
+                val navigatorHeight = ConvertUtils.dp2px(48f)
+
+//                val borderWidth = UIUtil.dip2px(context, 1.0).toFloat()
+                val lineHeight = navigatorHeight
+                indicator.lineHeight = lineHeight.toFloat()
+                indicator.roundRadius = ConvertUtils.dp2px(8f).toFloat()
+//                indicator.yOffset = borderWidth
+                indicator.setColors(Color.WHITE)
                 return indicator
             }
         }
         commonNavigator.isAdjustMode = true
         magicIndicator.navigator = commonNavigator
-
-        viewPaper.adapter = CaseDetailPagerAdapter(supportFragmentManager,case)
         ViewPagerHelper.bind(magicIndicator, viewPaper)
     }
 
     override val layoutId = R.layout.act_case_detail
 
     private val case by lazy { intent.getSerializableExtra(Param) as Case }
-    private val colorPrimary by lazy { ContextCompat.getColor(this,R.color.colorPrimary) }
+
+    private val colorPrimary by lazy { ContextCompat.getColor(this, R.color.colorPrimary) }
+
 }
