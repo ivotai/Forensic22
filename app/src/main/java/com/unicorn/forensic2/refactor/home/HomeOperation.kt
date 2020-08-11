1a84108b44333e11a691d46a8a1b86485e9f8635
ivotai <ivolianer2015@163.com>
2020-08-11 09:20:29 +0800

    4 待测试。

--- app/src/main/java/com/unicorn/forensic2/refactor/home/HomeOperation.kt
+++ app/src/main/java/com/unicorn/forensic2/refactor/home/HomeOperation.kt
@@ -4,13 +4,15 @@ import com.mikepenz.iconics.typeface.IIcon
 import com.unicorn.forensic2.app.role
 import com.unicorn.forensic2.data.model.Role
 import com.unicorn.forensic2.refactor.icon.Light
+import com.unicorn.forensic2.refactor.role.RoleWrapper
 
-enum class HomeOperation(val namez: String, val icon:IIcon) {
+enum class HomeOperation(val namez: String, val icon: IIcon) {
 
     JGCX("机构查询", Light.Icon.light_accessible_icon),
     XTGG("系统公告", Light.Icon.light_accessible_icon),
     TSJY("投诉建议", Light.Icon.light_accessible_icon),
     WDAJ("我的案件", Light.Icon.light_accessible_icon),
+    SPSX("审批事项", Light.Icon.light_accessible_icon),
     WDPS("我的评审", Light.Icon.light_accessible_icon),
     TZTX("通知提醒", Light.Icon.light_accessible_icon),
     JGXX("机构信息", Light.Icon.light_accessible_icon),
@@ -25,9 +27,19 @@ enum class HomeOperation(val namez: String, val icon:IIcon) {
                 Role.Normal -> listOf(JGCX, XTGG, TSJY, WDAJ)
                 Role.JdjgAdmin -> listOf(JGCX, XTGG, TSJY, WDAJ, TZTX, JGXX)
                 Role.Pszj -> listOf(JGXX, XTGG, TSJY, WDPS, TZTX, ZJXX)
-                Role.Spry,
-                Role.Sfjd,
-                Role.SfjdAdmin -> listOf(JGCX, XTGG, WDAJ, TZTX, ZTBG)
+                Role.Spry -> listOf(JGCX, XTGG, WDAJ, TZTX, ZTBG)
+                Role.Sfjd, Role.SfjdAdmin -> {
+                    val list = ArrayList<HomeOperation>()
+                    list.add(JGCX)
+                    list.add(XTGG)
+                    if (Role.Sfjd in RoleWrapper.SfjdMixture.roles)
+                        list.add(WDAJ)
+                    if (Role.SfjdAdmin in RoleWrapper.SfjdMixture.roles)
+                        list.add(SPSX)
+                    list.add(TZTX)
+                    list.add(ZTBG)
+                    list
+                }
                 else -> listOf(JGCX, XTGG)
             }
     }
--- app/src/main/java/com/unicorn/forensic2/refactor/home/HomeOperationAdapter.kt
+++ app/src/main/java/com/unicorn/forensic2/refactor/home/HomeOperationAdapter.kt
@@ -1,30 +1,23 @@
 package com.unicorn.forensic2.refactor.home
 
-import com.bumptech.glide.Glide
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.unicorn.forensic2.R
-import com.unicorn.forensic2.app.RxBus
-import com.unicorn.forensic2.app.safeClicks
-import com.unicorn.forensic2.app.setIIcon
-import com.unicorn.forensic2.app.startAct
+import com.unicorn.forensic2.app.*
 import com.unicorn.forensic2.data.event.SetCurrentItemEvent
-import com.unicorn.forensic2.data.model.HomeMenu
+import com.unicorn.forensic2.data.model.Role
 import com.unicorn.forensic2.ui.act.*
 import com.unicorn.forensic2.ui.act.list.PsxxListAct
 import com.unicorn.forensic2.ui.base.KVHolder
 import com.unicorn.forensic2.ui.operation.tztx.TztxAct
 import kotlinx.android.synthetic.main.item_home_menu.*
-import kotlinx.android.synthetic.main.item_home_menu.ivImg
-import kotlinx.android.synthetic.main.item_home_menu.root
-import kotlinx.android.synthetic.main.item_home_menu.tvName
-import kotlinx.android.synthetic.main.item_home_operation.*
 
-class HomeOperationAdapter : BaseQuickAdapter<HomeOperation, KVHolder>(R.layout.item_home_operation) {
+class HomeOperationAdapter :
+    BaseQuickAdapter<HomeOperation, KVHolder>(R.layout.item_home_operation) {
 
     override fun convert(helper: KVHolder, item: HomeOperation) {
         helper.apply {
             tvName.text = item.namez
-            ivImg.setIIcon(mContext,item.icon,R.color.colorPrimary)
+            ivImg.setIIcon(mContext, item.icon, R.color.colorPrimary)
         }
         helper.apply {
             root.safeClicks().subscribe {
@@ -36,7 +29,17 @@ class HomeOperationAdapter : BaseQuickAdapter<HomeOperation, KVHolder>(R.layout.
                     HomeOperation.JGXX -> mContext.startAct(JdjgMyGuideAct::class.java)
                     HomeOperation.PSXX -> mContext.startAct(PsxxListAct::class.java)
                     HomeOperation.ZJXX -> mContext.startAct(ExpertGuideAct::class.java)
-                    HomeOperation.WDAJ, HomeOperation.WDPS -> RxBus.post(SetCurrentItemEvent())
+                    HomeOperation.WDPS -> RxBus.post(SetCurrentItemEvent())
+                    HomeOperation.SPSX -> {
+                        if (role == Role.Sfjd)
+                            role = Role.SfjdAdmin
+                        RxBus.post(SetCurrentItemEvent())
+                    }
+                    HomeOperation.WDAJ -> {
+                        if (role == Role.SfjdAdmin)
+                            role = Role.Sfjd
+                        RxBus.post(SetCurrentItemEvent())
+                    }
                     else -> {
                     }
                 }
