2a3555e3641f7279337eaebd83c3b8b28aa4c902
ivotai <ivolianer2015@163.com>
2020-10-27 20:33:54 +0800

    添加提醒 fromFlag 可选择。

--- app/src/main/java/com/unicorn/forensic2/ui/operation/remind/AddRemindAct.kt
+++ app/src/main/java/com/unicorn/forensic2/ui/operation/remind/AddRemindAct.kt
@@ -1,10 +1,13 @@
 package com.unicorn.forensic2.ui.operation.remind
 
+import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materialdialogs.list.listItems
 import com.blankj.utilcode.util.ToastUtils
 import com.unicorn.forensic2.R
 import com.unicorn.forensic2.app.*
 import com.unicorn.forensic2.app.helper.DialogHelper
 import com.unicorn.forensic2.data.model.Case
+import com.unicorn.forensic2.data.model.Role
 import com.unicorn.forensic2.ui.base.BaseAct
 import io.reactivex.rxkotlin.subscribeBy
 import kotlinx.android.synthetic.main.act_remind_add.*
@@ -15,23 +18,43 @@ class AddRemindAct : BaseAct() {
 
     override fun initViews() {
         titleBar.setTitle("添加提醒")
-        tvFromFlag.text = "鉴定机构"
     }
 
     override fun bindIntent() {
         titleBar.setOperation("确定").safeClicks().subscribe {
             addRemind()
         }
+        tvFromFlag.safeClicks().subscribe { showDialog() }
+    }
+
+    private var mFromFlag = ""
+
+    private fun showDialog() {
+        val roles = if (role == Role.JdjgAdmin) listOf(Role.JdjgAdmin) else listOf(
+            Role.JdjgAdmin,
+            Role.Spry
+        )
+        MaterialDialog(this).show {
+            listItems(items = roles.map { it.cn }) { _, index, _ ->
+                val fromFlag = if (index == 0) "3" else "1"
+                mFromFlag = fromFlag
+                this@AddRemindAct.tvFromFlag.text = roles[index].cn
+            }
+        }
     }
 
     private fun addRemind() {
+        if (mFromFlag.isEmpty()) {
+            ToastUtils.showShort("请选择发送方")
+            return
+        }
         if (etRemark.isEmpty()) {
             ToastUtils.showShort("提醒内容不能为空")
             return
         }
         val mask = DialogHelper.showMask(this)
         val map = HashMap<String, RequestBody>()
-        map["fromFlag"] = "3".toRequestBody(TextOrPlain)
+        map["fromFlag"] = mFromFlag.toRequestBody(TextOrPlain)
         map["remark"] = etRemark.trimText().toRequestBody(TextOrPlain)
         map["toFlag"] = "2".toRequestBody(TextOrPlain)
         map["caseId"] = case.caseId.toRequestBody(TextOrPlain)
