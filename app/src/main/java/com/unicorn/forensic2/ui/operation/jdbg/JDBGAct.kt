edfb66ec6b2cc4ce722370fe1550616a67aebb62
ivotai <ivolianer2015@163.com>
2020-10-28 10:44:12 +0800

    修改鉴定报告界面

--- app/src/main/java/com/unicorn/forensic2/ui/operation/jdbg/JDBGAct.kt
+++ app/src/main/java/com/unicorn/forensic2/ui/operation/jdbg/JDBGAct.kt
@@ -14,15 +14,14 @@ class JDBGAct : BaseAct() {
     override fun initViews() {
         titleBar.setTitle("鉴定报告")
 
-        tvRemark.text = case.remark
-
         case.jdLotteryTime?.run {
-            tvDaysAppraisal.text = daysAppraisal.toString()
-            tvDaysPay.text = daysPay.toString()
-            tvDaysClearfee.text = daysClearfee.toString()
-            tvDaysCheck.text = daysCheck.toString()
-            tvDaysEvidence.text = daysEvidence.toString()
+            tvDaysAppraisal.setText(daysAppraisal.toString())
+            tvDaysPay.setText(daysPay.toString())
+            tvDaysClearfee.setText(daysClearfee.toString())
+            tvDaysCheck.setText(daysCheck.toString())
+            tvDaysEvidence.setText(daysEvidence.toString())
         }
+        tvRemark.setText(case.remark)
     }
 
     override fun bindIntent() {
